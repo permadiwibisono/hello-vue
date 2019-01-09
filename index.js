d454790dc61e81b749b2e7f3f870915e12503deb
Permadi Wibisono <mbapewe@gmail.com>
2019-01-10 01:28:58 +0700

    day01 - Method and Event Handler

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 new Vue({
   el: "#app",
   data: {
-    greeting: "Hello World",
+    greeting: "Hello World!",
     user: "Permadi Wibisono",
     city: "Indonesia"
+  },
+  methods: {
+    changeGreeting() {
+      this.greeting = this.greeting === 'Hello World!' ?
+      'What is up!' :
+      'Hello World!';
+    }
   }
 })
