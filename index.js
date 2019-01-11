0f6eea1ce2bc61ccf24c0da1c932c856c3d7d41b
Permadi Wibisono <mbapewe@gmail.com>
2019-01-11 13:28:47 +0700

    day02 - Data driven in vuejs

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 new Vue({
   el: "#app",
   data: {
-    // greeting: "Hello World!",
+    greeting: "",
     user: "Permadi Wibisono",
     city: "Indonesia"
   },
   methods: {
     addGreeting() {
-      this.greeting = 'Hello World';
+      this.greeting = 'Hello World!';
     },
     changeGreeting() {
       this.greeting = this.greeting === 'Hello World!' ?
