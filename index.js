c95ceedab4bb610c5e2ffed4ac55be418122304e
Permadi Wibisono <mbapewe@gmail.com>
2019-01-11 23:17:25 +0700

    day03 - Vue Directives (v-on:keyup) example.

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ new Vue({
   el: "#app",
   data: {
     greeting: "Hello World!",
+    alertMessage: "",
     user: "Permadi Wibisono",
     city: "Indonesia"
   },
   methods: {
+    alertMessageKeyUp() {
+      this.alertMessage = "You typed something!";
+    },
     changeGreeting() {
       this.greeting = this.greeting === 'Hello World!' ?
       'What is up!' :
