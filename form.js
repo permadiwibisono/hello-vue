f0e1efe886c7a02f502d29360fc068ef21d544f9
Permadi Wibisono <mbapewe@gmail.com>
2019-01-14 01:25:16 +0700

    day05 - Working with v-model

--- form.js
+++ form.js
@@ -1,7 +1,17 @@
 new Vue({
   el: "#app",
   data: {
+    name: '',
+    subject: '',
+    gender: 'Male',
+    termAndConditions: false
   },
   methods: {
+    onSubmit(){
+      console.log('name: ', this.name);
+      console.log('subject: ', this.subject);
+      console.log('gender: ', this.gender);
+      console.log('termAndConditions: ', this.termAndConditions);
+    }
   }
 })
