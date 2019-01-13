ddf6b420dfcf769dacf613eee9799df7b7ad8a4d
Permadi Wibisono <mbapewe@gmail.com>
2019-01-14 01:35:48 +0700

    day05 - Using v-for when building option

--- form.js
+++ form.js
@@ -3,7 +3,14 @@ new Vue({
   data: {
     name: '',
     subject: '',
+    subjectList: [
+      { label:'Engineering', value: 'Engineering' },
+      { label: 'Computer Science', value: 'Computer Science' },
+      { label: 'Biology', value: 'Biology' },
+      { label: 'Other...', value: 'Other' },
+    ],
     gender: 'Male',
+    genderList: ['Female', 'Male'],
     termAndConditions: false
   },
   methods: {
